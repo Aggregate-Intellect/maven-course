d001104e04be68a218886b5f8bc766e2066c3bfa
Abhimanyu Anand <63829842+abhimanyu729@users.noreply.github.com>
2024-09-07 13:33:13 -0400

    creating repo structure for the assignments and adding demo agent for week 1

---
+++ assignments/week_01/langgraph-demo-agent/agent.py
@@ -0,0 +1,9 @@
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import create_react_agent
+
+model = ChatOpenAI(model="gpt-4o")
+
+tools = [TavilySearchResults(max_results=2)]
+
+graph = create_react_agent(model, tools)
